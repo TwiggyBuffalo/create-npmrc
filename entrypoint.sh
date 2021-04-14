66b979f28106c1963bf0b6fc183927e7548c1fdc
Daniel Curran <danielcurran@live.co.za>
2021-04-14 13:39:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
-echo "registry=https://registry.npmjs.org/" > .npmrc
-echo "@$INPUT_ORG_NAME:registry = https://npm.pkg.github.com/" >> .npmrc
+echo "registry=https://registry.npmjs.org/" > "$HOME/.npmrc"
+echo "@$INPUT_ORG_NAME:registry = https://npm.pkg.github.com/" >> "$HOME/.npmrc"
 if [[ -z "$INPUT_AUTH_TOKEN" ]]; then
-    echo "//npm.pkg.github.com/:_authToken=$AUTH_TOKEN" >> .npmrc
+    echo "//npm.pkg.github.com/:_authToken=$AUTH_TOKEN" >> "$HOME/.npmrc"
 else
-    echo "//npm.pkg.github.com/:_authToken=$INPUT_AUTH_TOKEN" >> .npmrc
+    echo "//npm.pkg.github.com/:_authToken=$INPUT_AUTH_TOKEN" >> "$HOME/.npmrc"
 fi
